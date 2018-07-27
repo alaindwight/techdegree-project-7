e7456dfaf29c94fbd4689ba188ad35c0d01a8ceb
AJ <alainkassabian@gmail.com>
2018-07-27 13:29:27 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const qwerty = document.getElementById('qwerty');
+const phrase = document.getElementById('qwerty');
+const start = document.getElementsByClassName('btn__reset');
+let missed = 0;
+const phrases = [
+    "Luck favors the bold",
+    "Only the strong survive",
+    "Pain is temporary",
+    "Discipline is freedom",
+    "Patience is a virtue"
+];
+
+$( start ).click(function() {
+    document.getElementById('overlay').style.display = 'none';
+});
+
+//
+// JS START BUTTONS THAT DON'T WORK... Why????
+//
+
+//start.addEventListener('click', () => {
+    
+  //  document.getElementById('overlay').style.display = 'none';
+//});
+
+//(function startGame() {
+//    document.getElementById('overlay').style.display = 'none';
+//})();
+
+
+//start.addEventListener('click', startGame()
+//);
