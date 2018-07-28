2ce9457365c111796cd950ed5d0e9d75ee7d28d2
AJ <alainkassabian@gmail.com>
2018-07-27 21:49:18 -0400

    Letters are added to display as liste items

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let missed = 0;
 let choosePhrase = Math.floor(Math.random() * 5);
 const phrases = [
     "Luck favors the bold",
-    "Only the strong survive",
+    "Strive through life",
     "Pain is temporary",
     "Discipline is freedom",
     "Patience is a virtue"
@@ -41,18 +41,55 @@ $( start ).click(function() {
 // PHRASE ARRAY
 //
 
+
 function getRandomPhraseArray(arr){
     console.log(choosePhrase);
     console.log(arr[choosePhrase]);
     let phraseLength = arr[choosePhrase].length;
     console.log(phraseLength);
+    const phraseLetters = [];
 
     for (i = 0; i < phraseLength; i++) {
-        var phraseLetters = [];
-        phraseLetters[i] = arr[choosePhrase].substr(i, 1);
+        
+        phraseLetters.push(arr[choosePhrase].substr(i, 1));
         console.log(phraseLetters[i]);
     }
 
-    //function returns NEW ARRAY???????
+    return phraseLetters;
 }
-getRandomPhraseArray(phrases);
+
+const phraseLetters = getRandomPhraseArray(phrases);
+
+console.log(getRandomPhraseArray(phrases));
+
+
+function addPhraseToDisplay(arr){
+
+    for (i = 0; i < phraseLetters.length; i++) {
+        if (!arr[i].trim()) {
+            var listItem = document.createElement("LI");
+            var text = document.createTextNode(phraseLetters[i]);
+            listItem.appendChild(text);
+            document.getElementById("phrase-list").appendChild(listItem);            
+        }
+        else {
+            var listItem = document.createElement("LI");
+            var text = document.createTextNode(phraseLetters[i]);
+            listItem.appendChild(text);
+            document.getElementById("phrase-list").appendChild(listItem).className = "letter";
+        }
+    }
+}
+
+addPhraseToDisplay(phraseLetters);
+
+function checkLetter (arr){
+    for (i = 0; i < phraseLetters.length; i++) {
+        if (arr == phraseLetters[i]) {
+            //apply show class to element
+        }
+        else{
+            return null;
+        }
+    }
+}
